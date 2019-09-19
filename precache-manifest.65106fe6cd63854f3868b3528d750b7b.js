4ce897a6d6e2427c5f23b27c6d4b04289b372d96
carrany <roy.nthiga@smart.local>
2019-09-19 16:10:44 +0300

    Updates

---
+++ precache-manifest.65106fe6cd63854f3868b3528d750b7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f48841d33169aae807eba6e8a97f86",
+    "url": "/wallet/index.html"
+  },
+  {
+    "revision": "334e98bc2d28e426d189",
+    "url": "/wallet/static/css/2.3f74805b.chunk.css"
+  },
+  {
+    "revision": "096dadcb2faf97b38090",
+    "url": "/wallet/static/css/main.4729085b.chunk.css"
+  },
+  {
+    "revision": "334e98bc2d28e426d189",
+    "url": "/wallet/static/js/2.889a1046.chunk.js"
+  },
+  {
+    "revision": "096dadcb2faf97b38090",
+    "url": "/wallet/static/js/main.fcde17cf.chunk.js"
+  },
+  {
+    "revision": "3dbc71d3c2eeaf1f42f1",
+    "url": "/wallet/static/js/runtime~main.626a16bc.js"
+  },
+  {
+    "revision": "a3a974aab90bf14f6803a84712074f30",
+    "url": "/wallet/static/media/logo.a3a974aa.jpg"
+  }
+]);
